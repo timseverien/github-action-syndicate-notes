aa36f8bdee2bebc9f81b86689d4cf60c6b7ed7b9
Tim Severien <timseverien@gmail.com>
2024-09-17 21:55:50 +0200

    feat: remove message

--- src/index.ts
+++ src/index.ts
@@ -101,7 +101,6 @@ try {
 
 	// Cache is empty — this could be first run, so let's persist cache so next run has a starting point
 	if (cache.syndicatedItems.size === 0) {
-		core.info('This looks like your first run!');
 		await persistCache(cacheDirectory, cache);
 	}
 
